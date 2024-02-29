022ca309ac0eadb549822c9e9c9e0bfe147a216e
darkness8129 <yaroslaw460@gmail.com>
2024-02-29 21:27:50 +0200

    feat(post): add entity structure

--- app/entity/post.go
+++ app/entity/post.go
@@ -1,3 +1,18 @@
 package entity
 
-type Post struct{}
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	ID string `json:"id" gorm:"type:uuid;primaryKey;default:uuid_generate_v4()"`
+
+	Title   string `json:"title,omitempty"`
+	Content string `json:"content,omitempty"`
+
+	CreatedAt time.Time      `json:"createdAt,omitempty" gorm:"index"`
+	UpdatedAt time.Time      `json:"updatedAt,omitempty"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt,omitempty"`
+}
